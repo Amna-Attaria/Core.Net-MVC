3b72fe89576ac45c64c2f9a74e6b5b6d4e3792ec
Amna-Attaria <khan12amna26@gmail.com>
2025-04-22 22:42:46 +0500

    Email Sending

---
+++ email Sending/email Sending/Controllers/feedback.cs
@@ -0,0 +1,48 @@
+﻿using email_Sending.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR.Protocol;
+using System.Net;
+using System.Net.Mail;
+
+namespace email_Sending.Controllers
+{
+    public class feedback : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(mail ee)
+        {
+            int id = ee.Id;
+            string cusemail = ee.email;
+          string cusmessage = ee.message;
+            
+            ViewBag.cusemail = cusemail;
+            ViewBag.cusmessage = cusmessage;
+
+
+            /////////////////////// email code ///////////////////
+            ///
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            client.EnableSsl = true;
+            client.Credentials = new NetworkCredential("amna12khan26@gmail.com", "ravy hvfw tzoc ckde");
+
+            //////create the email message
+            MailMessage msg = new MailMessage();
+            msg.From = new MailAddress("amna12khan26@gmail.com");
+            msg.To.Add(cusemail.ToString());
+
+            msg.Subject = "Feedback";
+            msg.Body = cusmessage.ToString();
+
+            client.Send(msg);
+
+            ViewBag.message = "Mail Send Successfully";
+            return View();
+        }
+    }
+}
---
+++ email Sending/email Sending/Models/mail.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace email_Sending.Models
+{
+    public class mail
+    {
+        [Key]
+
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter you email")]
+        [EmailAddress]
+        [StringLength(100)]
+        public string email {  get; set; }
+
+        [Required(ErrorMessage = "Please enter you MESSAGE")]
+        [StringLength(120)]
+        public string message { get; set; }
+    }
+}
